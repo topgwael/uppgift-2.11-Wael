e7ee8356b9fe0d0697d86eb66b3b6b51f3476a40
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:53:45 +0200

    Add project files.

---
+++ uppgift 2.11 Wael/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hur långt hoppade Elin?");
+            string x = Console.ReadLine();
+            int h = int.Parse(x);
+            Console.WriteLine("hur långt hoppade Alma?");
+            string a = Console.ReadLine();
+            int k = int.Parse(a);
+            int s = h - k;
+            Console.WriteLine("skjilnaden mellan Elin och Alma är  " + s);
+        }
+
+
+    }
+
+}
